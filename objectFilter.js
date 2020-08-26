0e8fdc402ce31c0b565191a8ba6dbf877dd0617f
Ai Mi Bui <aimibui.dev@gmail.com>
2020-08-26 13:01:33 -0700

    format

--- objectFilter.js
+++ objectFilter.js
@@ -1,26 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-syntax */
 // Challenge 11
 
-// Construct a function objectFilter that accepts an object as the first parameter 
-// and a callback function as the second parameter. 
-// objectFilter will return a new object. 
-// The new object will contain only the properties from the input object 
+// Construct a function objectFilter that accepts an object as the first parameter
+// and a callback function as the second parameter.
+// objectFilter will return a new object.
+// The new object will contain only the properties from the input object
 // such that the property's value is equal to the property's key passed into the callback.
 
 const cities = {
-London: 'LONDON',
-LA: 'Los Angeles',
-Paris: 'PARIS',
+  London: 'LONDON',
+  LA: 'Los Angeles',
+  Paris: 'PARIS',
 };
 
-
-function objectFilter(obj, callback){
-    let newObj = {}
-    for (let key in obj) {
-        if (callback(key) === obj[key]){
-            newObj[key] = obj[key]
-        }
+function objectFilter(obj, callback) {
+  const newObj = {};
+  for (const key in obj) {
+    if (callback(key) === obj[key]) {
+      newObj[key] = obj[key];
     }
-    return newObj;
+  }
+  return newObj;
 }
 
-console.log(objectFilter(cities, city => city.toUpperCase())) // Should log { London: 'LONDON', Paris: 'PARIS'}
+console.log(objectFilter(cities, city => city.toUpperCase())); // Should log { London: 'LONDON', Paris: 'PARIS'}

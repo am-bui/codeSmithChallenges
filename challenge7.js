ef84689468b9678e86a2503a5ed7eda90777c652
Ai Mi Bui <aimibui.dev@gmail.com>
2020-08-26 12:57:37 -0700

    format

--- challenge7.js
+++ challenge7.js
@@ -1,5 +1,5 @@
-// Construct a function intersection that compares input arrays 
-// and returns a new array with elements found in all of the inputs. 
+// Construct a function intersection that compares input arrays
+// and returns a new array with elements found in all of the inputs.
 // BONUS: Use reduce!
 
 // input: array of arrays
@@ -22,32 +22,33 @@
 // return new array
 // return reduce(array, callback, initialValue)
 
-function intersection(...arrayOfArrays){
-
-    function reduce(array, callback, initialValue){
-        let accumulator = initialValue; 
-        for (let i = 1; i < array.length; i++){ 
-            const current = array[i];
-            accumulator = callback(accumulator, current)
-        } 
-        return accumulator
+function intersection(...arrayOfArrays) {
+  function reduce(array, callback, initialValue) {
+    let accumulator = initialValue;
+    for (let i = 1; i < array.length; i += 1) {
+      const current = array[i];
+      accumulator = callback(accumulator, current);
     }
+    return accumulator;
+  }
 
-    function filter(array1, array2){
-        const newArray = [];
-        for (let i = 0; i < array1.length; i++){
-            const element = array1[i] 
-            if (array2.includes(element)) {
-                newArray.push(element)
-            }
-        }
-        return newArray
+  function filter(array1, array2) {
+    const newArray = [];
+    for (let i = 0; i < array1.length; i += 1) {
+      const element = array1[i];
+      if (array2.includes(element)) {
+        newArray.push(element);
+      }
     }
+    return newArray;
+  }
 
-    return reduce(arrayOfArrays, filter, arrayOfArrays[0])
+  return reduce(arrayOfArrays, filter, arrayOfArrays[0]);
 }
 
-console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])); // should log: [5, 15]
+console.log(
+  intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
+); // should log: [5, 15]
 
 // function intersection(arrayOfArrays){}
 // const arr1 = [5, 10, 15, 20];

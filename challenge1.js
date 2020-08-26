ec742394e813e5f72d3b17211bb1651d4c923220
Ai Mi Bui <aimibui.dev@gmail.com>
2020-08-26 12:53:05 -0700

    format

--- challenge1.js
+++ challenge1.js
@@ -1,8 +1,8 @@
 // Challenge 1
 // Create a function addTwo that accepts one input and adds 2 to it.
 
-function addTwo (num) {
-    return num + 2;
-};
+function addTwo(num) {
+  return num + 2;
+}
 
 console.log(addTwo(4));

c19b7c755cc219d4011fa60710257f59fafa129c
Ai Mi Bui <aimibui.dev@gmail.com>
2020-08-26 12:55:40 -0700

    format

--- challenge4.js
+++ challenge4.js
@@ -1,18 +1,18 @@
 // Challenge 4
-// The function forEach takes an array and a callback, 
-// and runs the callback on each element of the array. 
+// The function forEach takes an array and a callback,
+// and runs the callback on each element of the array.
 // forEach does not return anything.
 
 let alphabet = '';
 const letters = ['a', 'b', 'c', 'd'];
 
-function forEach(array, callback){
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i])
-    }
+function forEach(array, callback) {
+  for (let i = 0; i < array.length; i += 1) {
+    callback(array[i]);
+  }
 }
 
 forEach(letters, function(char) {
   alphabet += char;
 });
-console.log(alphabet);   //prints 'abcd'
+console.log(alphabet); // prints 'abcd'

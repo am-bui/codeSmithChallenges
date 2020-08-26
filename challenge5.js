7086ed5584e85642849b660a040592ac0d8e9932
Ai Mi Bui <aimibui.dev@gmail.com>
2020-08-26 10:17:59 -0700

    update map

--- challenge5.js
+++ challenge5.js
@@ -1,28 +1,7 @@
-// Challenge 5
-
-// In the first part of this challenge, you're going to rebuild map as mapWith. 
-// This time you're going to use forEach inside of mapWith instead of using a for loop.
-
-
-
-// declare a mapWith function that takes an array and a callback
-// declare a variable called result and set it to an empty array
-// instead of iterating with a for loop, invoke forEach
-// forEach will take an array as the 1st param
-// the 2nd param will be function that wraps the evaluation of the invocation of the callback on each element and pushes it into result
-// return result
-
-function mapWith(array, callback){
-    const result = [];
-    forEach(array, el => result.push(callback(el)))
-    return result;
-}
-
-
-// declare a forEach function 
-// forEach will take an array and a callback as params
-// it will iterate through the array
-// and run the callback on each element of the array
+// Part 1
+// Create a function forEach which takes an array and a callback, 
+// and runs the callback on each element of the array. forEach does not return anything. 
+// Please do not use the native forEach or map method.
 
 function forEach(array, callback) {
     for (let i = 0; i < array.length; i++) {
@@ -30,15 +9,21 @@ function forEach(array, callback) {
     }
 }
 
+// Part 2
+// Now let's rebuild map
+// This time instead of using a for loop, you're going to use the forEach we just created.
 
-let alphabet = '';
-const letters = ['a', 'b', 'c', 'd'];
-forEach(letters, function(char) {
-  alphabet += char;
-});
-console.log(alphabet); //prints 'abcd'
+function map(array, callback) {
+    const result = [];
+    function processAndAddToResult(element) {
+      const processedElement = callback(element);
+      result.push(processedElement);
+    }
+    forEach(array, processAndAddToResult);
+    return result;
+}
 
 console.log(typeof forEach); // should log: 'function'
 forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
-console.log(typeof mapWith); // should log: 'function'
-console.log(mapWith([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+console.log(typeof map); // should log: 'function'
+console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]

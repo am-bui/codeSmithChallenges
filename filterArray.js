20cbe5650cb8d2242d7ca4673196d64796e4102b
Ai Mi Bui <aimibui.dev@gmail.com>
2020-09-01 06:59:15 -0700

    completed challege

---
+++ filterArray.js
@@ -0,0 +1,23 @@
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [1, 2, 3, 4, 5];
+
+function func1(num) {
+  if (num % 2 === 0) {
+    return num;
+  }
+}
+function func2(num) {
+  if (num % 2 !== 0) {
+    return num;
+  }
+}
+
+// Uncomment these to check your work!
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]

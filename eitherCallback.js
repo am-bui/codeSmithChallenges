9e4b90fe2ad3221da01b63150a802d884eb50a07
Ai Mi Bui <aimibui.dev@gmail.com>
2020-09-01 21:28:01 -0700

    v2 solution

--- eitherCallback.js
+++ eitherCallback.js
@@ -3,7 +3,7 @@
 // simply passing the two callbacks into eitherFilter in the previous challenge.
 
 function eitherCallback(callback1, callback2) {
-  function newCallback(currentElement) {
+  function mergedCallbacks(currentElement) {
     let value = currentElement;
     if (callback1(value)) {
       value = callback1(value);
@@ -12,7 +12,7 @@ function eitherCallback(callback1, callback2) {
     }
     return value;
   }
-  return newCallback;
+  return mergedCallbacks;
 }
 
 function filterArray(array, callback) {
@@ -24,10 +24,21 @@ function filterArray(array, callback) {
 }
 
 const arrOfNums = [10, 35, 105, 9];
-
 const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
 const over100 = n => n > 100;
-
 const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
 
 console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
+
+/*
+v.2
+
+function eitherCallback(callback1, callback2) {
+  return function mergedCallback(...args) {
+    if (callback1(args[0])) {
+      return true;
+    }
+    if (callback2(args[0])) return true;
+  };
+}
+ */

c107d03c17ac9c2c0095af508cb6b553610fd86a
Ai Mi Bui <aimibui.dev@gmail.com>
2020-09-22 13:02:14 -0700

    alt solution/refactor

--- factorial.js
+++ factorial.js
@@ -2,22 +2,45 @@
 
 // EXAMPLE4! = 4 * 3 * 2 * 1 = 24, so calling factorial(4) should return 24.
 
-// Input: {Number} num - number whose factorial is sought
-// Output: {Number}
+// To get the factorial of a number you multiply that number by itself minus one until you reach the number one.
 
-function factorial(num, product = 1) {
-  // termination case: if num is less than 0, return to prevent recusion from running
-  if (num < 0) return `Please input a positive integer.`;
-  // base case: when num has reach 0, return the product
-  if (num === 0) return product;
-  // declare a variable that multiplies num by the product
-  const multiplyProduct = num * product;
-  // declare a variable that decrements num
-  const decNum = num - 1;
-  // return the evaluated result of using recursion, calling factorial with updated inputs
-  return factorial(decNum, multiplyProduct);
+function factorial(num) {
+  if (num < 0) {
+    return 'Please input a positive integer';
+  }
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 }
 
-console.log(factorial(4));
-console.log(factorial(-1));
-console.log(factorial(6));
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(factorial(5)); // -> 120
+console.log(factorial(4)); // -> 24
+console.log(factorial(6)); // -> 720
+console.log(factorial(0)); // -> 1
+
+/* 
+        First Part of the recursion method
+        You need to remember that you won’t have just one call, you’ll have several nested calls
+        
+        Each call: num === "?"        	         num * factorialize(num - 1)
+        1st call – factorialize(5) will return    5  * factorialize(5 - 1) // factorialize(4)
+        2nd call – factorialize(4) will return    4  * factorialize(4 - 1) // factorialize(3)
+        3rd call – factorialize(3) will return    3  * factorialize(3 - 1) // factorialize(2)
+        4th call – factorialize(2) will return    2  * factorialize(2 - 1) // factorialize(1)
+        5th call – factorialize(1) will return    1  * factorialize(1 - 1) // factorialize(0)
+        
+        Second part of the recursion method
+        The method hits the if condition, it returns 1 which num will multiply itself with
+        The function will exit with the total value
+        
+        5th call will return (5 * (5 - 1))     // num = 5 * 4
+        4th call will return (20 * (4 - 1))    // num = 20 * 3
+        3rd call will return (60 * (3 - 1))    // num = 60 * 2
+        2nd call will return (120 * (2 - 1))   // num = 120 * 1
+        1st call will return (120)             // num = 120
+        
+        If we sum up all the calls in one line, we have
+        (5 * (5 - 1) * (4 - 1) * (3 - 1) * (2 - 1)) = 5 * 4 * 3 * 2 * 1 = 120
+ */

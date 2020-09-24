b64caa2b5593a51c55df11e1862bb86f213accd0
Ai Mi Bui <aimibui.dev@gmail.com>
2020-09-23 20:50:56 -0700

    new solution

--- sum.js
+++ sum.js
@@ -2,30 +2,23 @@
 /* eslint-disable no-plusplus */
 // Write a recursive function sum that calculates the sum of an array of integers.
 
-function sum(array, index = 0, acc = 0) {
-  if (!array[index]) {
-    return acc;
-  }
-  let accSum = acc;
-  accSum += array[index++];
-  return sum(array, index, accSum);
+function sum(array) {
+  if (array.length === 0) return 0;
+  return array[0] + sum.slice(1);
 }
 
 console.log(sum([1, 1, 1])); // -> returns 3
 console.log(sum([1, 2, 3, 4, 5, 6])); // -> returns 21
 
 /* 
-with comments:
+prev solution:
 
-function sum(array, index = 0, accSum = 0) {
-  // if there is no element in the array at the index, return acc
+function sum(array, index = 0, acc = 0) {
   if (!array[index]) {
-    return accSum;
+    return acc;
   }
-  // reassign acc to the value of adding the array at the current index to the accumulated value
-  // increment index after add value into acc
-  acc += array[index++];
-  // return sum(array, index, accSum)
+  let accSum = acc;
+  accSum += array[index++];
   return sum(array, index, accSum);
 }
 */

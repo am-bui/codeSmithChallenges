557fe070e8e465cd798a881321400994b62e777f
Ai Mi Bui <aimibui.dev@gmail.com>
2020-09-25 08:10:55 -0700

    refactor

--- getLength-array.js
+++ getLength-array.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
 // Get the length of an array using recursion without accessing its length property.
 
 // Input: {Array} array - array whose length is sought
 // Output: {Number}
 
+// base case: if array at the index is undefined, return index
+// we have to increase index before the recursion because we want to count the existing elements
+// increment the index
+// recursively call getLength on the array and the updated index
+
 function getLength(array, index = 0) {
-  // base case: if array at the index is undefined, return index
   if (!array[index]) {
     return index;
   }
-  // we have to increase index before the recursion because we want to count the existing elements
-  // increment the index and store the value in a variable
-  const incIndex = index + 1;
-  // recursively call getLength on the array and the updated index
-  return getLength(array, incIndex);
+  index++;
+  return getLength(array, index);
 }
 
 console.log(getLength([1])); // -> 1

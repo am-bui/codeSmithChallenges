0a55f187a4ffcba94e94dc46eb9884e62d31a775
Ai Mi Bui <aimibui.dev@gmail.com>
2020-10-10 20:40:36 -0700

    refactor

--- once.js
+++ once.js
@@ -1,13 +1,11 @@
-// Write a function once that accepts a callback as input and returns a function.
-// When the returned function is called the first time, it should call the
-// callback and return that output. If it is called any additional times,
-// instead of calling the callback again it will simply return the output
-// value from the first time it was called.
-
-const addByTwoOnce = once(function(num) {
-  return num + 2;
-});
-
-// UNCOMMENT THESE TO TEST YOUR WORK!
-console.log(addByTwoOnce(5)); // should log 7
-console.log(addByTwoOnce(10)); // should log 7
+function once(callback) {
+  let hasBeenRun = false;
+  let firstReturnedValue;
+  return callBackArg => {
+    if (!hasBeenRun) {
+      firstReturnedValue = callback(callBackArg);
+      hasBeenRun = true;
+    }
+    return firstReturnedValue;
+  };
+}
